83f02f2ff259ca6536936bf391cfc386fa18cf91
universalmind <universalmind.candy@gmail.com>
2017-09-25 22:23:34 -0500

    rpi

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from flask import Flask, send_file
 import os
+
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 app = Flask(__name__)
@@ -12,15 +14,24 @@ def index():
 def grocery():
   return send_file('templates/grocery.html')
 
+@app.route("/cardcast")
+def cardcast():
+  return send_file('templates/cardcast.html')
+
 @app.errorhandler(404)
 def page_not_found(e):
     """Return a custom 404 error."""
     return 'Sorry, nothing at this URL.', 404
 
+@app.errorhandler(500)
+def server_error(e):
+    """Return custom 500 error."""
+    return ' 500 status error', 500
+
 
 # make sure this gets disabled before deploying
-# if __name__ == '__main__':
-#   app.run(host='0.0.0.0')
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=8080, debug=True)
 
 
 STATIC_URL = '/static/'
--- static/javascripts/portfolio.js
+++ static/javascripts/portfolio.js
@@ -1,8 +1,8 @@
-angular.module('portfolio', ['requests'])
+angular.module('portfolio', ['requests',])
 
 .controller('portfolioCtrl', function($scope, Request, $location) {
 
   $scope.cardcast = () => location = 'http://corygrinstead.com/cardcast';
   $scope.groceryBag = () => location = 'http://corygrinstead.com/grocerybag';
-  $scope.github = () => location = 'http://github.com/universalmind303';
+  $scope.github = () => location = 'https://github.com/my-sentry/my-sentry';
 });
